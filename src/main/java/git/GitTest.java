22d4ad05a53d5ab1e19b7bf86d15586944f97c7f
yang <yang@163.com>
2022-04-17 23:51:28 +0800

    second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -6,5 +6,7 @@ package git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
