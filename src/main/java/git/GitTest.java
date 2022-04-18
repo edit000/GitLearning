3fcb7e0d91e3eec70541b9321182758a49f8a36e
yang <yang@163.com>
2022-04-18 10:46:20 +0800

    third commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -8,5 +8,8 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
