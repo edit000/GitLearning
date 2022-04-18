51c0744720fd49a3dff93afde403d40d146a115f
yang <yang@163.com>
2022-04-18 11:05:07 +0800

    master commit1

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -5,6 +5,8 @@ package git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
