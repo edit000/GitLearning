9d09ddf825c24899e7a768f86b4270a3e5fb09c6
yang <yang@163.com>
2022-04-18 10:54:20 +0800

    hot-fix test

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -5,6 +5,8 @@ package git;
  */
 public class GitTest {
     public static void main(String[] args) {
+
         System.out.println("Hello Git");
+        System.out.println("Hello Git hot-fix");
     }
 }
